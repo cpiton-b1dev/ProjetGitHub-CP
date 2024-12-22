ebcd8949c599426c660bb1bf2c7fc79bc5065a56
cpiton-b1dev <cpiton.b1dev@gmail.com>
2024-12-22 18:03:01 +0100

    modification index.php et ajout login.php

--- index.php
+++ index.php
@@ -6,16 +6,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="login.css">
-    <title>Accueil</title>
+    <title>Accueil - Titre modifié</title>
 </head>
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main>
         <div class="px-4 py-5 my-5 text-center">
             <h1 class="display-5 fw-bold text-body-emphasis">Bienvenue</h1>
             <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-                    lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices
-                    diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.</p>
+                <p class="lead mb-4">Nouveau paragraphe - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
+                    lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
                 <div>
                     <a href="login.php" class="btn btn-outline-primary btn-lg px-4 active">Page de connexion</a>
                 </div>
